295efd93fd0fbb0c88e76c2445d1345f396cfd54
sashpawar11 <saisaipawar@gmail.com>
2021-10-01 12:25:23 +0530

    ContactDetailsFeature

---
+++ Main/sendcontact.php
@@ -0,0 +1,41 @@
+
+<?php
+
+//sql connection to database for getting contact details
+$conn = mysqli_connect("localhost", "root", "", "gslchatbot") or die("Database Connection Error!");
+
+$getAns = mysqli_real_escape_string($conn,$_POST['text']);
+
+$check_query = "SELECT dept_name, dept_email, dept_phone FROM department_details WHERE opt_id LIKE '%$getAns%'";
+$execute_query = mysqli_query($conn,$check_query) or die("Error while executing query!");
+
+if(mysqli_num_rows($execute_query) > 0){
+    $fetch_data = mysqli_fetch_assoc($execute_query);
+
+    echo "
+    <div class='bot-inbox inbox'>
+        <div class='icon'>
+            <i class='fas fa-robot'></i>
+        </div>
+        <div class='msg-header'>
+            <p>Department Name:<br>{$fetch_data['dept_name']}<br>
+             Department Email:<br>{$fetch_data['dept_email']}<br>
+            Department Phone:<br>{$fetch_data['dept_phone']}<br></p>
+        </div>
+    </div>";
+
+   //echo $reply;
+}else{ 
+
+        // When a user will ask a question which the bot cannot answer,
+        // it will store it in a seperate datatable called newqueries.
+        // so that later the bot can be trained to answer that question.
+    $insert_query = "INSERT INTO newqueries (que) VALUES('$getAns')";
+    $run_query = mysqli_query($conn, $insert_query) or die("Error while inserting into database!");
+    echo "Sorry, I'm not able to understand your query.";
+}
+
+
+
+
+?>
--- Main/src/script.js
+++ Main/src/script.js
@@ -1,74 +1,97 @@
 $(document).ready(function(){
 
 
+    // msgloader on opening chat window 
+    $('.loader').delay(2500).fadeOut();
+
     TODO : 'TIMESTAMP FOR POSTS'
-    // var curr_time = new Date();
-    // var timestamp = curr_time.getHours + ":" + curr_time.getMinutes;
-    // document.write(datetime);
+//    const post_time = getCurrentTime();
+//    document.getElementById("msg_time").innerHTML = post_time;
 
 
-
+   let contactMode = false;
    var chatwindow = document.getElementsByClassName("wrapper")[0];
     $("#send-btn").on("click", function(){
         $value = $("#data").val();
         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
-        $(".form").append($msg);
+        $loader = '<div class="loader"></div>';
+        $(".form").append($msg,$loader);
+        $('.loader').delay(700).fadeOut();
         $("#data").val('');
         
+
+        // General User Queires
         // Ajax code
-        $.ajax({
-            url: 'sendmsg.php',
-            // url: 'sendmsg.php',
-            method: 'POST',
-            headers: {
-                "Access-Control-Allow-Methods" : "OPTIONS, GET, POST, PUT, DELETE",
-                // 'Access-Control-Allow-Headers': 'POST',
-                // 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-                // 'Access-Control-Allow-Credentials': 'true',
-                // // header('Access-Control-Max-Age:86400');
-                // 'Access-Control-Allow-Headers' :' Origin, Content-Type, X-Auth-Token,  Accept, Authorization, X-Requested-With',
-                'Access-Control-Allow-Origin': '*'
-                
-             },
-            data: 'text='+$value,
-            success: function(result){
-                $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-robot"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
-                $(".form").append($reply);
-                $(".form").scrollTop($(".form")[0].scrollHeight);
-            }
+        
+        if(contactMode == false)
+        {
+
+            setTimeout(function(){
+                $.ajax({
+                    url: 'sendmsg.php',
+    
+                    method: 'POST',
+                    headers: {
+                        "Access-Control-Allow-Methods" : "OPTIONS, GET, POST, PUT, DELETE",
+                        'Access-Control-Allow-Origin': '*'
+                        
+                    },
+                    data: 'text='+$value,
+                    success: function(result){
+                        $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-robot"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($reply);
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                    
+                });
             
-        });
+        },1500);
+    }
+
+        // CONTACT QUERIES
+        if(contactMode == true)
+        {
+            setTimeout(function(){
+                
+                $.ajax({
+                url: 'sendcontact.php',
+    
+                method: 'POST',
+                headers: {
+                    "Access-Control-Allow-Methods" : "OPTIONS, GET, POST, PUT, DELETE",
+                    'Access-Control-Allow-Origin': '*'
+                    
+                 },
+                data: 'text='+$value, 
+                success: function(result){
+                    //$reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-robot"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                    $reply = result;
+                    $(".form").append($reply);
+                    $(".form").scrollTop($(".form")[0].scrollHeight);
+                    contactMode = false;
+                
+                }
+                
+            });
+        }, 1500);
+            
+        };
+
     });
 
+
     $("#contact").on("click", function(){
+        contactMode = true;
         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Contact Details</p></div></div>';
-        
-        $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>So you need the contacts huh</p></div></div>';
-        $(".form").append($msg,$reply);
-        // $(".form").scrollTop($(".form")[0].scrollHeight);
-        // Ajax code
-        // $.ajax({
-        //     url: 'sendmsg.php',
-        //     // url: 'sendmsg.php',
-        //     method: 'POST',
-        //     headers: {
-        //         "Access-Control-Allow-Methods" : "OPTIONS, GET, POST, PUT, DELETE",
-        //         // 'Access-Control-Allow-Headers': 'POST',
-        //         // 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        //         // 'Access-Control-Allow-Credentials': 'true',
-        //         // // header('Access-Control-Max-Age:86400');
-        //         // 'Access-Control-Allow-Headers' :' Origin, Content-Type, X-Auth-Token,  Accept, Authorization, X-Requested-With',
-        //         'Access-Control-Allow-Origin': '*'
-                
-        //      },
-        //     data: 'text='+$value,
-        //     success: function(result){
-        //         $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
-        //         $(".form").append($reply);
-        //         $(".form").scrollTop($(".form")[0].scrollHeight);
-        //     }
-            
-        // });
+        $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-robot"></i></div><div class="msg-header"><p>Which of the following department details do you need? (Reply with option):<br>1.IT-ERP<br>2.IT-EDP<br>3.Ship Repair</p></div></div>';
+        $loader = '<div class="loader"></div>';
+        $(".form").append($msg,$loader);
+        $('.loader').delay(700).fadeOut();
+
+        setTimeout(function(){
+            $(".form").append($reply);
+        }, 1500);
+
     });
 
     $("#chatbtn").on("click", function()
@@ -102,20 +125,24 @@ $(document).ready(function(){
     // setTimeout(function(){ location.reload(); }, 10);   // Add this to refresh the page on clicking close, does not save the conversation.
     });
 
-    // Delay for displaying Message1
+    // Delay for displaying Message1  $('#botmsg').delay
     setTimeout(function(){
         document.getElementById('botmsg').style.visibility = "visible";
-        },3500);
+        },3000);
 
 
         // Delay for displaying Options
     setTimeout(function(){
         document.getElementById('opt_inbox').style.visibility = "visible";
-        },2500);
-       
-    // msgload anim dissapear delay    
-    $('.loader').delay(3200).fadeOut();
-
+        },3800);
 
 });
 
+// function getCurrentTime()
+// {
+
+//     var curr_time = new Date();
+//     var timestamp = curr_time.getHours() + ":" + curr_time.getMinutes();
+//     return timestamp;
+
+// }
