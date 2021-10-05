ef16014ab4f3c08eb6e191364491231ba13dc091
sashpawar11 <saisaipawar@gmail.com>
2021-10-05 12:32:12 +0530

    UI Images+ Audiomsg

--- Main/src/script.js
+++ Main/src/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 
+    // Resolution responsive variables
+    var h = window.innerHeight;
+    var w = window.innerWidth;
 
     // msgloader on opening chat window 
     $('.loader').delay(2500).fadeOut();
-
+    
 
     // Greeting User Msg on Doc load
     greetUser();
@@ -18,14 +21,16 @@ $(document).ready(function(){
 
    
     $("#send-btn").on("click", function(){
-        $value = $("#data").val();
+        
+        $value = $("#data").val().trim();  // Removing Whitespaces around input-data string.
         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
         // $loader = '<div class="loader"></div>';
         // $(".form").append($msg,$loader);
         // $('.loader').delay(700).fadeOut();
+        
         getLoaderAnimation($msg);
         $("#data").val('');
-        
+        $(".form").scrollTop($(".form")[0].scrollHeight);
 
         // General User Queires
         // Ajax code
@@ -35,7 +40,7 @@ $(document).ready(function(){
 
             setTimeout(function(){
                 $.ajax({
-                    url: 'sendmsg.php',
+                    url: 'messagesquery.php',
     
                     method: 'POST',
                     headers: {
@@ -46,10 +51,11 @@ $(document).ready(function(){
                     data: 'text='+$value,
                     success: function(result){
                         i++;
-                        $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-robot"></i></div><div class="msg-header"><p>'+ result +'</p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
+                        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header"><p>'+ result +'</p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
                         $(".form").append($reply);
+                        playMsgSound();
                         setTimeStamp(i);
-
+                        
                         $(".form").scrollTop($(".form")[0].scrollHeight);
                     }
                     
@@ -64,7 +70,7 @@ $(document).ready(function(){
             setTimeout(function(){
                 
                 $.ajax({
-                url: 'sendcontact.php',
+                url: 'contactquery.php',
     
                 method: 'POST',
                 headers: {
@@ -79,6 +85,7 @@ $(document).ready(function(){
                     i++;
                     $(".form").append($reply);
                     $(".form").scrollTop($(".form")[0].scrollHeight);
+                    playMsgSound();
                     setTimeStamp(i);
                     contactMode = false;
                     
@@ -100,36 +107,52 @@ $(document).ready(function(){
         
         contactMode = true;
         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Contact Details</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
-        $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-robot"></i></div><div class="msg-header"><p>Which of the following department details do you need? (Reply with option):<br>1.IT-ERP<br>2.IT-EDP<br>3.Ship Repair</p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
+        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header"><p>Which of the following department details do you need? (Reply with option):<br>1.IT-ERP<br>2.IT-EDP<br>3.Ship Repair</p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
         i++;
         getLoaderAnimation($msg,$reply);
         setTimeout(function(){
             setTimeStamp(i);
         }, 1501);
+        $(".form").scrollTop($(".form")[0].scrollHeight);
     });
 
     $("#info").on("click", function(){
         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Info</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
-        $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-robot"></i></div><div class="msg-header" style="max-width: 65%;"><p style="word-break: normal;">Goa Shipyard Limited (GSL) established in 1957, is a leading ISO 9001-2015 certified shipyard on the West Coast of India, functioning under the administrative control of Ministry of Defence, Govt. of India.</p><p style="word-break: break-all;">GSL is strategically located on the banks of river Zuari in Goa, a major international tourist destination well connected by its international airport and major port enroute all important shipping lines.</p><p style="word-break: break-all;">Beginning as a small barge building yard, GSL has garnered reputation as one of the most sophisticated ship builders in the Country.</p><p><img src="https://goashipyard.in/file/2016/08/GSL-Products.jpg" alt="GSL-Yard" width="180" height="120"></img></p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
+        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header" style="max-width: 65%;"><p style="word-break: normal;">Goa Shipyard Limited (GSL) established in 1957, is a leading ISO 9001-2015 certified shipyard on the West Coast of India, functioning under the administrative control of Ministry of Defence, Govt. of India.</p><p style="word-break: break-all;">GSL is strategically located on the banks of river Zuari in Goa, a major international tourist destination well connected by its international airport and major port enroute all important shipping lines.</p><p style="word-break: break-all;">Beginning as a small barge building yard, GSL has garnered reputation as one of the most sophisticated ship builders in the Country.</p><p><img src="https://goashipyard.in/file/2016/08/GSL-Products.jpg" alt="GSL-Yard" width="180" height="120"></img></p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
         i++;
         getLoaderAnimation($msg,$reply);
         setTimeout(function(){
             setTimeStamp(i);
         }, 1501);
+        $(".form").scrollTop($(".form")[0].scrollHeight);        // scrolling to bottom of the  form
 
     });
 
     $("#location").on("click", function(){
 
         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Location</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
-        $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-robot"></i></div><div class="msg-header" style="max-width: 68%;"><p><iframe width="200" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=200&amp;height=200&amp;hl=en&amp;q=Goa%20Shipyard%20Limited,%20Vaddem,%20Vasco-da-gama,%20Goa,%20Inida+(Goa%20Shipyard%20Limited)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
+        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header" style="max-width: 68%;"><p><iframe width="200" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=200&amp;height=200&amp;hl=en&amp;q=Goa%20Shipyard%20Limited,%20Vaddem,%20Vasco-da-gama,%20Goa,%20Inida+(Goa%20Shipyard%20Limited)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
         i++;
         getLoaderAnimation($msg,$reply);
         setTimeout(function(){
             setTimeStamp(i);
         }, 1501);
 
+        $(".form").scrollTop($(".form")[0].scrollHeight);
 
+    });
+
+    $('#query').on("click", function(){
+
+        $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Other Queries</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
+        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header"><p>Please state your query.</p></div></div>';
+        i++;
+        getLoaderAnimation($msg,$reply);
+        setTimeout(function(){
+            setTimeStamp(i);
+        }, 1501);
+
+        $(".form").scrollTop($(".form")[0].scrollHeight);
     });
 
 
@@ -172,22 +195,25 @@ $(document).ready(function(){
 
     $("#maxbtn").on("click", function(){
 
+        var maxminheight = (h-120) + 'px';
         $( ".wrapper" ).animate({
             opacity: 1,
             // top: "+=0",
-            height: "100%",
-            width: "100%",
-          
+            width: '100%',
+            bottom: "0",
+            right: "0",         
             
           }, {
               duration: 20,
               easing: "linear",
           }, function(){});
-          $(".form").css('min-height' ,'500px');
-          $(".form").css('max-height' ,'550px');
+        //   autoResizeWrapper();
+          $(".form").css('min-height' , maxminheight);
+          $(".form").css('max-height' , maxminheight);
           $(this).css('display', 'none');
           $("#minbtn").css('display', 'inline-block');
           $(".input-data").css('width','600px');
+         
 
     });
 
@@ -196,16 +222,17 @@ $(document).ready(function(){
         $( ".wrapper" ).animate({
             opacity: 1,
             // top: "+=0",
-            height: "70%",
+            // height: "fit-content",
             width: "360px",
             easing: "linear",
           }, 100, function() {
             
           });
   
-          $(".wrapper").css('width' ,'360px');
+        //   $(".wrapper").css('width' ,'360px');
           $(".wrapper").css('right' ,'6px');
           $(".wrapper").css('bottom' ,'4px');
+          $(".wrapper").css('height', 'fit-content');
           $(this).css('display', 'none');
           $("#maxbtn").css('display', 'inline-block');
           $(".form").css('min-height' ,'350px');
@@ -250,17 +277,18 @@ function greetUser()
     var greetmsg = document.getElementById("greet");
     if( hr >= 0 && hr < 12)
     {
-        greetmsg.innerHTML = "Good Morning User!<br>How can I help you?";
+        greetmsg.innerHTML = "Good Morning User!<br>How can I help you today?";
     }
     else if( hr >= 12 && hr < 17)
     {
-        greetmsg.innerHTML = "Good Afternoon User!<br>How can I help you?";
+        greetmsg.innerHTML = "Good Afternoon User!<br>How can I help you today?";
 
     }
     else
     {
-        greetmsg.innerHTML = "Good Evening User!<br>How can I help you?";
+        greetmsg.innerHTML = "Good Evening User!<br>How can I help you today?";
     }
+    playMsgSound();
 
 }
 
@@ -275,6 +303,7 @@ function getLoaderAnimation($msg,$reply)
 
     setTimeout(function(){
         $(".form").append($reply);
+        playMsgSound();
     }, 1500);
 
 }
@@ -292,4 +321,18 @@ function setTimeStamp(i)
     minutes = minutes < 10 ? '0'+minutes : minutes;
     var timestamp =  hrs + ":" + minutes + " " + am_pm;
     document.getElementsByClassName("msg_time")[i].innerHTML = timestamp;
+}
+
+
+function autoResizeWrapper()
+{
+
+    document.getElementsByClassName("wrapper")[0].style.height = window.innerHeight + 'px';
+
+}
+
+function playMsgSound()
+{
+
+    document.getElementById("msg_notif").play();
 }
