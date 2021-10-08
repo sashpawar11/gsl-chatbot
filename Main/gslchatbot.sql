5a91b2aad90fbe10625c01b75a259b10eb3988c7
sashpawar11 <saisaipawar@gmail.com>
2021-10-08 12:02:11 +0530

    ContactUs,Collapse,Nav

--- Main/gslchatbot.sql
+++ Main/gslchatbot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2021 at 12:51 PM
+-- Generation Time: Oct 08, 2021 at 07:24 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -63,7 +63,8 @@ INSERT INTO `knowledge_base` (`id`, `questions`, `answers`) VALUES
 (1, 'Hello', 'Hi!'),
 (2, 'What is your name?', 'My name is GSL-chatbot'),
 (3, 'Hi', 'Hello!'),
-(4, 'Thanks|Thank you|Thankyou', 'It was great helping you, goodbye user!');
+(4, 'Thanks|Thank you|Thankyou', 'It was great helping you, goodbye user!'),
+(5, 'Who is the current cmd? | CMD | Who is the head | Current CMD | cmd', 'Cmde B.B. Nagpal, NM,IN(Retd) - Chairman & Managing Director');
 
 -- --------------------------------------------------------
 
@@ -83,13 +84,7 @@ CREATE TABLE `newqueries` (
 INSERT INTO `newqueries` (`ID`, `que`) VALUES
 (7, 'What is your name '),
 (9, 'What is your age'),
-(16, 'Hello '),
-(17, 'What is your name!'),
-(19, 'fqf'),
-(20, 'cvv'),
-(21, 'Hrllo'),
-(22, 'What is your name!'),
-(23, 'Hello');
+(25, 'What is your name!');
 
 --
 -- Indexes for dumped tables
@@ -121,13 +116,13 @@ ALTER TABLE `newqueries`
 -- AUTO_INCREMENT for table `knowledge_base`
 --
 ALTER TABLE `knowledge_base`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `newqueries`
 --
 ALTER TABLE `newqueries`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Main/src/script.js
+++ Main/src/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    // Resolution responsive variables
+    // Storing window resolution for different screen sizes
     var h = window.innerHeight;
     var w = window.innerWidth;
 
@@ -11,7 +11,7 @@ $(document).ready(function(){
     // Greeting User Msg on Doc load
     greetUser();
 
-    TODO : 'TIMESTAMP FOR POSTS'
+    // 'TIMESTAMP FOR POSTS'
     var i = 0;
     setTimeStamp(i);
    
@@ -103,10 +103,10 @@ $(document).ready(function(){
     //////////////////////////////////////////////////
     // Question Option Functions
 
-    $("#contact").on("click", function(){
+    $("#dept").on("click", function(){
         
         contactMode = true;
-        $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Contact Details</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
+        $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Department Details</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
         $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header"><p>Which of the following department details do you need? (Reply with option):<br>1.IT-ERP<br>2.IT-EDP<br>3.Ship Repair</p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
         i++;
         getLoaderAnimation($msg,$reply);
@@ -118,7 +118,7 @@ $(document).ready(function(){
 
     $("#info").on("click", function(){
         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Info</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
-        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header" style="max-width: 65%;"><p style="word-break: normal;">Goa Shipyard Limited (GSL) established in 1957, is a leading ISO 9001-2015 certified shipyard on the West Coast of India, functioning under the administrative control of Ministry of Defence, Govt. of India.</p><p style="word-break: break-all;">GSL is strategically located on the banks of river Zuari in Goa, a major international tourist destination well connected by its international airport and major port enroute all important shipping lines.</p><p style="word-break: break-all;">Beginning as a small barge building yard, GSL has garnered reputation as one of the most sophisticated ship builders in the Country.</p><p><img src="https://goashipyard.in/file/2016/08/GSL-Products.jpg" alt="GSL-Yard" width="180" height="120"></img></p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
+        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header" style="max-width: 65%;"><p style="word-break: normal;"><b>Goa Shipyard Limited (GSL)</b> established in 1957, is a leading ISO 9001-2015 certified shipyard on the West Coast of India, functioning under the administrative control of Ministry of Defence, Govt. of India.</p><p style="word-break: break-all;">GSL is strategically located on the banks of river Zuari in Goa, a major international tourist destination well connected by its international airport and major port enroute all important shipping lines.</p><p style="word-break: break-all;">Beginning as a small barge building yard, GSL has garnered reputation as one of the most sophisticated ship builders in the Country.</p><p><img src="https://goashipyard.in/file/2016/08/GSL-Products.jpg" alt="GSL-Yard" width="180" height="120"></img></p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
         i++;
         getLoaderAnimation($msg,$reply);
         setTimeout(function(){
@@ -156,7 +156,33 @@ $(document).ready(function(){
     });
 
 
+    $("#contactUs").on("click", function(){
 
+        $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Contact Us</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
+        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header" style="max-width: 68%;"><p>VASCO-DA-GAMA, GOA <br>-403802 </p><p>Phone: <br>+91-832-2512152 (5 LINES/ 2513954 / 2512359 </p><p>Email:<a href="mailto:contactus@goashipyard.com"  >contactus@goashipyard.com</a></p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
+        i++;
+        getLoaderAnimation($msg,$reply);
+        setTimeout(function(){
+            setTimeStamp(i);
+        }, 1501);
+
+        $(".form").scrollTop($(".form")[0].scrollHeight);
+        
+    });
+
+    $("#navigation").on("click", function(){
+        
+    
+        $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>Department Details</p></div><div class="timestamps"><i class="fa fa-check-double m-1" style="font-size:8pt; color:#5C7AEA;"></i></div></div>';
+        $reply = '<div class="bot-inbox inbox"><div class="icon"><img src="img/bot.png" width="24" height="24"></div><div class="msg-header"><p>Where should I redirect you to (Reply with option):<br>1.Open Tenders<br>2.Facilites<br>3.Services<br>4.Gallery</p></div><div class="timestamps"><p class="msg_time">0:00</p></div></div>';
+        i++;
+        
+        getLoaderAnimation($msg,$reply);
+        setTimeout(function(){
+            setTimeStamp(i);
+        }, 1501);
+        $(".form").scrollTop($(".form")[0].scrollHeight);
+    });
 
 
     /////////////////////////
@@ -183,34 +209,56 @@ $(document).ready(function(){
             // top: "+=0",
             height: "toggle",
             width: "toggle",
-          }, 100, function() {
-            
+          }, 200, function() {
+            document.getElementById("chatbtn").style.display = "block";
+            document.body.style.overflow = 'auto';
+            setTimeout(function(){ location.reload(); }, 10);   // Add this to refresh the page on clicking close, does not save the conversation.
+            });
           });
-        // ​$('.wrapper').css('display'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'block');​​​​​​
-    //chatwindow.style.display="none";
-    document.getElementById("chatbtn").style.display = "block";
-    document.body.style.overflow = 'auto';
-    setTimeout(function(){ location.reload(); }, 10);   // Add this to refresh the page on clicking close, does not save the conversation.
-    });
+    
 
     $("#collapseBtn").on("click", function()
     {
         $( ".wrapper" ).animate({
             opacity: 1,
             // top: "+=0",
-            height: "6%",
+            height: "6.2%",
            
           }, 100, function() {
             
           });
         // ​$('.wrapper').css('display'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'block');​​​​​​
     //chatwindow.style.display="none";
-    $("#collapseBtn").style.visibility= "hidden";
-    $("#restore_collapseBtn").style.visibility = "visible";
-    document.getElementById("chatbtn").style.display = "block";
+    $(this).css('display', 'none');
+    $("#restore_collapseBtn").css('display', 'inline');
+    // document.getElementById("chatbtn").style.display = "block";
     document.body.style.overflow = 'auto';
     
     });
+
+
+    $("#restore_collapseBtn").on("click", function()
+    {
+        
+         
+          $( ".form" ).animate({
+            opacity: 1,
+            // top: "+=0",
+            height: "350px",
+          }, 200, function() {
+            
+          });
+          $(".wrapper").css('height', 'fit-content');
+         
+          $(".form").css('min-height' ,'350px');
+          $(".form").css('max-height' ,'350px');
+          $("#collapseBtn").css('display', 'inline');
+          $(this).css('display', 'none');
+    }
+    
+    
+    
+    );
     $("#maxbtn").on("click", function(){
 
         var maxminheight = (h-120) + 'px';
@@ -231,6 +279,7 @@ $(document).ready(function(){
           $(this).css('display', 'none');
           $("#minbtn").css('display', 'inline-block');
           $(".input-data").css('width','600px');
+          document.getElementsByClassName('navbar')[0].classList.remove('fixed-top');
           document.body.style.overflow = 'hidden'; // Locking Body Scroll
          
 
@@ -257,6 +306,7 @@ $(document).ready(function(){
           $(".form").css('min-height' ,'350px');
           $(".form").css('max-height' ,'350px');
           $(".input-data").css('width','335px');
+          document.getElementsByClassName('navbar')[0].classList.add('fixed-top');
           document.body.style.overflow = 'auto';
     });
 
@@ -288,6 +338,8 @@ $(document).ready(function(){
 });
 
 
+
+// FUNCTION TO GREET USER ACCORDING TO THE CURRENT TIME
 function greetUser()
 {
 
@@ -296,7 +348,7 @@ function greetUser()
     var greetmsg = document.getElementById("greet");
     if( hr >= 0 && hr < 12)
     {
-        greetmsg.innerHTML = "Good Morning User!<br>How can I help you today?";
+        greetmsg.innerHTML = "<b>Good Morning User!</b><br>How can I help you today?";
     }
     else if( hr >= 12 && hr < 17)
     {
@@ -309,11 +361,12 @@ function greetUser()
     }
     playMsgSound();
 
+
 }
 
 
 
-
+/// FUNCTION TO GET THE LOADER SPINNER ANIMATION FOR BOT MESSAGE DELAYS
 function getLoaderAnimation($msg,$reply)
 {
     $loader = '<div class="loader"></div>';
@@ -328,6 +381,8 @@ function getLoaderAnimation($msg,$reply)
 }
 
 
+
+// FUNCTION TO SET TIMESTAMPS FOR THE BOT MESSAGES
 function setTimeStamp(i)
 {
    
