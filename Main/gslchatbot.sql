5843427907f34c4dd5661c5a7dab391779f1e78d
sashpawar11 <saisaipawar@gmail.com>
2021-10-25 10:21:28 +0530

    projreportchanges

--- Main/gslchatbot.sql
+++ Main/gslchatbot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2021 at 08:53 AM
+-- Generation Time: Oct 25, 2021 at 05:38 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
